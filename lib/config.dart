441de15a11bd2592dc1dd18179da522692ffd70e
Daniel Kin <daniel.ivatiuk@gmail.com>
2022-12-03 21:34:32 -0300

    micro core initial setup

---
+++ lib/config.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+typedef WidgetBuilderArgs = Widget Function(BuildContext context, Object? args);
+GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
